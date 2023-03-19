e44cd5e227d2532ec828375e01fd874ea8b82cac
Late515 <s2201201@edu.bc.fi>
2023-03-19 17:05:21 +0200

    "edetty osaan 2:10, Custom events and emitter"

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { COURSES } from "src/db-data";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
+
+  @Output("courseSelected")
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCourseViewed() {
+    console.log("card component - button clicked");
+
+    this.courseEmitter.emit(this.course);
+  }
+}
