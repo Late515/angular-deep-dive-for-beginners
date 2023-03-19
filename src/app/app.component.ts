6660880ddbd948c6556ec65e62abeb94aab3f520
Late515 <s2201201@edu.bc.fi>
2023-03-19 18:48:29 +0200

    "pipes and stuff 2 Section vika osa"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ import { Course } from "./model/course";
 export class AppComponent {
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  startDate = new Date(2000, 0, 1);
+
+  price = 9.99;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
   onCourseSelected(course: Course) {
     console.log("App component - click event bubbled...", course);
   }
